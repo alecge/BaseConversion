2fd2f421b0a7c47f080444d300bc6d1c8c1f65b8
Alec Ge <alecge1998@gmail.com>
2016-09-19 10:45:45 -0400

    binary to decimal complete

--- src/org/alecge/Main.java
+++ src/org/alecge/Main.java
@@ -1,8 +1,25 @@
 package org.alecge;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// syntax: Main.class <value to convert> <base of value> <base to convert to>
+
+        if(args[1] == '2' && args[2] == '10')
+            System.out.println("Binary to Decimal: " + binaryToDecimal(args[0]));
+    }
+    public static int binaryToDecimal(String binary){
+        double decimal = 0;
+
+        for(int i = binary.length(); i > 0; i--){
+            if(binary.charAt(i-1) == '1')
+                decimal += Math.pow(2, i-1);
+            else
+                decimal +=0;
+        }
+
+        return (int) decimal;
     }
 }
